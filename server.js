399ea2e1c0d3f95b9e300cc3dbffb3ac911837a4
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-12-27 18:09:44 +0000

    creat pages and routes for individual menu items

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const axios = require("axios").default;
 let path = require('path');
-const port = 3000;
+const port = 5000;
 
 let ejs = require('ejs');
 
@@ -20,23 +20,8 @@ app.use(express.static('./public'));
 // create routes 
 // display homepage 
 app.get('/', (req, res)=> {
-    
-    let external_api_data;
-
-    axios.get("https://api-afrikorture.glitch.me/vendors")
-    .then(function (response){
-        external_api_data = response.data
-        // console.log(external_api_data);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-
     //render home page
     res.render('pages/index');
-
-    // res.send(json(external_api_data));
 });
 
 // display create shop html page 
@@ -44,12 +29,12 @@ app.get('/create-shop', (req, res)=>{
     res.render('pages/register-store');
 })
 
-// display brand directory page 
+// display brand directory html page 
 app.get('/brand-directory', (req, res)=>{
     res.render('pages/brand-directory');
 })
 
-//send data for for brand directory page
+//send data for brand directory page
 app.get('/all-shops', (req, res)=>{
     // get data from external api 
     axios.get("https://api-afrikorture.glitch.me/stores")
@@ -67,6 +52,136 @@ app.get('/all-shops', (req, res)=>{
         console.log(err);
     })
     
+})
+
+// women related routing starts =========================================
+// display this html page for women accessories
+app.get('/women-accessories', (req, res)=>{
+    res.render('pages/women-accessories');
+})
+
+//get data from external api for women accessories page
+// app.get('/women-accessories-data', (req, res)=>{
+//     // get data from external api 
+//     axios.get("https://api-afrikorture.glitch.me/women-accessories")
+//     .then((response)=>{
+//         let all_stores_data = response["data"];
+//         console.log(all_stores_data);
+
+//         // filter the data and get only the data that is needed by the user 
+//         let needed_store_data=[];
+//         all_stores_data.forEach((store)=>{
+//             needed_store_data.push({store_name: store["store_name"], county:store["county"], address:store["store_address"]});
+//         })
+//         res.send(needed_store_data);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
+    
+// })
+
+// display this html page for women shirts and blouses
+app.get('/women-shirt-&-blouse', (req, res)=>{
+    res.render('pages/women-shirt-&-blouse');
+})
+
+// display this html page for women trouser and pants
+app.get('/women-trousers-&-pants', (req, res)=>{
+    res.render('pages/women-trousers-&-pants');
+})
+
+// display this html page for women dresses and skirts
+app.get('/women-dresses-&-skirts', (req, res)=>{
+    res.render('pages/women-dresses-&-skirts');
+})
+
+// display this html page for women shirts and blouse
+app.get('/women-footwear', (req, res)=>{
+    res.render('pages/women-footwear');
+})
+
+// men related routing starts =========================================
+// display this html page for men accessories
+app.get('/men-accessories', (req, res)=>{
+    res.render('pages/men-accessories');
+})
+
+// display this html page for men shirts and blouses
+app.get('/men-shirt', (req, res)=>{
+    res.render('pages/men-shirt');
+})
+
+// display this html page for men trouser and pants
+app.get('/men-trousers-&-pants', (req, res)=>{
+    res.render('pages/men-trousers-&-pants');
+})
+
+// display this html page for men shirts and blouse
+app.get('/men-footwear', (req, res)=>{
+    res.render('pages/men-footwear');
+})
+
+// kids related routing starts =========================================
+// display this html page for kids accessories
+app.get('/kids-accessories', (req, res)=>{
+    res.render('pages/kids-accessories');
+})
+
+// display this html page for kids shirts and blouses
+app.get('/kids-shirt-&-blouse', (req, res)=>{
+    res.render('pages/kids-shirt-&-blouse');
+})
+
+// display this html page for kids trouser and pants
+app.get('/kids-trousers-&-pants', (req, res)=>{
+    res.render('pages/kids-trousers-&-pants');
+})
+
+// display this html page for kids dresses and skirts
+app.get('/kids-dresses-&-skirts', (req, res)=>{
+    res.render('pages/kids-dresses-&-skirts');
+})
+
+// display this html page for kids shirts and blouse
+app.get('/kids-footwear', (req, res)=>{
+    res.render('pages/kids-footwear');
+})
+
+
+// beauty related routing starts =========================================
+// display this html page for beauty accessories
+app.get('/beauty-accessories', (req, res)=>{
+    res.render('pages/beauty-accessories');
+})
+
+// display this html page for body & bath
+app.get('/body-and-bath', (req, res)=>{
+    res.render('pages/body-and-bath');
+})
+
+// display this html page for kids accessories
+app.get('/hair-care', (req, res)=>{
+    res.render('pages/hair-care');
+})
+
+// display this html page for kids accessories
+app.get('/skin-care', (req, res)=>{
+    res.render('pages/skin-care');
+})
+
+
+// apartment related routing starts =========================================
+// display this html page for home decoration products
+app.get('/home-decoration', (req, res)=>{
+    res.render('pages/home-decoration');
+})
+
+
+// eat related routing starts =========================================
+// display this html page for snacks
+app.get('/snacks', (req, res)=>{
+    res.render('pages/snacks');
 })
 
 
