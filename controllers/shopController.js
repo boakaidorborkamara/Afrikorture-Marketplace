671b770df992247c30188396fe7673ed8dcdf344
Boakai <boakaidkamara@gmail.com>
2022-04-26 07:11:21 +0000

    displaying form that enable users to create new shop

---
+++ controllers/shopController.js
@@ -0,0 +1,4 @@
+exports.displayBrandCretionForm = ((req, res)=>{
+    res.render('pages/create-brand')
+    // res.send("Working on displaying shop form");
+})
--- routers/shop_routes.js
+++ routers/shop_routes.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const { reset } = require("nodemon");
 const router = express.Router();
 
-router.get("/create-shop", (req,res)=>{
-    res.send("Working on creating and displaying shops");
-});
+
+//SHOP CONTROLLERs
+const shopController = require('../controllers/shopController');
+
+
+//Display ejs page for creating new shops on GET
+router.get("/create-shop", shopController.displayBrandCretionForm);
+
+//Create a new shop on POST
+router.post("/create-shop", (req,res)=>{
+    res.send("Working on creating a new shop")
+})
+
+//Display all the shops on the platform
+router.get("/all-shops", (req,res)=>{
+    res.send("Working on displaying all the shops in db");
+})
 
 module.exports = router;
